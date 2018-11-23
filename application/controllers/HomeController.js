8f220f47dca19e272068a29ec53a89eb09a5c3b0
Your Name <you@example.com>
2018-11-23 13:29:44 +0530

    home page companies updated

--- application/controllers/HomeController.js
+++ application/controllers/HomeController.js
@@ -1,8 +1,13 @@
 const jobs = require("../data/jobs.json");
 const categories = require("../data/categories.json");
+const companies = require("../data/companies.json");
 class HomeController {
   static get(req, res, next) {
-    res.render("index", { jobs: jobs, categories: categories });
+    res.render("index", {
+      jobs: jobs,
+      categories: categories,
+      companies: companies
+    });
   }
 }
 
