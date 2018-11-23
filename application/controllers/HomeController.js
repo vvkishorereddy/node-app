d01946d1103d14b7ced4f62813f9584f81364bb3
Your Name <you@example.com>
2018-11-23 16:04:53 +0530

    more routes adding

--- application/controllers/HomeController.js
+++ application/controllers/HomeController.js
@@ -1,6 +1,7 @@
 const jobs = require("../data/jobs.json");
 const categories = require("../data/categories.json");
 const companies = require("../data/companies.json");
+
 class HomeController {
   static get(req, res, next) {
     res.render("index", {
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const exphbs = require("express-handlebars");
 const app = express();
 
 const HomeRoutes = require("./application/routes/HomeRoute");
+const JobsRoutes = require("./application/routes/JobsRoute");
 
 // variables
 const PORT = process.env.PORT || 8080;
@@ -26,6 +27,7 @@ app.use(
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
 // routes
+app.use("/jobs", JobsRoutes);
 app.use(HomeRoutes);
 
 // app start
